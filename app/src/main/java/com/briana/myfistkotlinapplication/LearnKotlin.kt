7debbcb28bef3c0b01eb5f4f09fa8b00f529f8fa
Briana-cxw <brianacxw@163.com>
2020-12-29 18:04:09 +0800

    20201229

---
+++ app/src/main/java/com/briana/myfistkotlinapplication/LearnKotlin.kt
@@ -0,0 +1,12 @@
+package com.briana.myfistkotlinapplication
+
+/**
+ * Create by Briana-cxw
+ * 2020/12/28 10:51
+ */
+fun main(){
+    var p = Person()
+    p.name = "Jack"
+    p.age = 19
+    p.eat()
+}
---
+++ app/src/main/java/com/briana/myfistkotlinapplication/Person.kt
@@ -0,0 +1,13 @@
+package com.briana.myfistkotlinapplication
+
+/**
+ * Create by Briana-cxw
+ * 2020/12/28 14:43
+ */
+class Person {
+    var name = ""
+    var age = 0
+    fun eat(){
+        println(name + "is eating. He is "+ age + " years old")
+    }
+}
